db97392f1c13fef5f0612dd18271dd139afcceb1
Ziya SARIKAYA <ziya.sarikaya@here.com>
2016-12-14 10:27:26 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public static class ArrayExtensions{
+         public static void Populate<T>(this T[] arr, T value ) {
+            for ( int i = 0; i < arr.Length;i++ ) {
+                arr[i] = value;
+            }
+        }
+    }
+    public class Program
+    {
+        private  const int windowSize = 200000;
+        private  const int msgCount = 1000000;
+        private  const int msgSize = 1024;
+        private static long worst = 0;
+
+        private static byte[] createMessage(int n)
+        {
+            byte[] msg = new byte[msgSize];
+            msg.Populate((byte)n);
+            return msg;
+        }
+       
+        private static void pushMessage( byte[][] store, int id)
+        {
+            long start = DateTime.Now.Ticks;
+            store[id % windowSize] = createMessage(id);
+            long elapsed = DateTime.Now.Ticks - start;
+            if (elapsed > worst)
+            {
+                worst = elapsed;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            byte[][] store = new byte[windowSize][];
+            for (int i = 0; i < msgCount; i++)
+            {
+                pushMessage(store, i);
+            }
+
+            Console.WriteLine("Worst push time: " + (worst / 1000000.0f));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # csharp-gc-latency-experiment
+
 C# GC Latency Experiment
+
+> Event tracing for Windows (ETW) that provides [detailed information](https://msdn.microsoft.com/en-us/library/ff356162.aspx)
+
+Here are the steps in using the tool to get the GC information;
+
+- Start cmd as admin
+- Start the application
+- Start monitoring `PerfMonitor.exe /process:{your-process-id} start`
+- do stuff
+- Stop monitoring `PerfMonitor.exe stop`
+- Geenrate report `PerfMonitor.exe GCTime` This command will generate a report you need_
